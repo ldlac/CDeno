3f54f30709805fa8f94e0e744ea6911a934798f3
ldlac <ldlacerte1@gmail.com>
2020-06-16 23:39:34 -0400

    .

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import { serve } from 'https://deno.land/std/http/server.ts';
  
 const server = serve({ port: 8000 });
  
-for await (const req of server) {
-  req.respond({ body: 'Hello Deno' });
-}
+(async function() {
+  for await (const req of server) {
+    req.respond({ body: 'Hello Deno' });
+  }
+})();
